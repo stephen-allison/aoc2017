3ba58fef61c3effa773fc134740e728b4cdb8cc8
Stephen Allison <git@steve.allison.clara.co.uk>
2021-08-23 23:52:12 +0100

    completed 3b

--- src/aoc2107/core.clj
+++ src/aoc2107/core.clj
@@ -103,8 +103,35 @@
   (let [[end-x end-y] (nth (spiral-path) (dec end-square))]
     (println "day 3a " (+ (Math/abs end-x) (Math/abs end-y)))))
 
-(nth (spiral-path) 6)
+(def day3-input 325489)
 
-(day3-1 325489)
+(day3-1 day3-input)
 
-(nth (reductions move [0 0] (spiral-path)) 2)
+(def up-right (comp up right))
+
+(def up-left (comp up left))
+
+(def down-right (comp down right))
+
+(def down-left (comp down left))
+
+(def directions [right up-right up up-left left down-left down down-right])
+
+(defn neighbours [pos] (map #(% pos) directions))
+
+(defn cell-value [cells pos]
+  (let [adjacent (neighbours pos)
+        vals (map #(get cells % 0) adjacent)]
+    (if (= [0 0] pos) 1
+        (apply + vals))))
+
+(defn cell-value-and-max [[cells max-val] pos]
+  (let [new-val (cell-value cells pos)]
+    [(assoc cells pos new-val) (max max-val new-val)]))
+
+(defn day3-2 [target]
+  (let [values (reductions cell-value-and-max [{} 0] (spiral-path))
+        [_ v] (first (drop-while (fn [[_ v]] (< v target)) values))]
+    (println "day 3b " v)))
+
+(day3-2 day3-input)
