23499532de3d29a1bbbf42252b7078f8edaa27dc
Stephen Allison <git@steve.allison.clara.co.uk>
2021-08-30 21:27:22 +0100

    counter reimplemented

--- src/util/counter.clj
+++ src/util/counter.clj
@@ -1,18 +1,12 @@
 (ns util.counter)
 
-(defn inc-count [counts value]
+(defn- inc-count [counts value]
   (if (contains? counts value) 
     (update counts value inc)
     (assoc counts value 1)))
 
 (defn count-items [items-to-count]
-  (loop [items items-to-count
-         counts {}]
-    (let [value (first items)
-          remaining (rest items)]
-      (if (empty? remaining)
-        (inc-count counts value)
-        (recur remaining (inc-count counts value))))))
+  (reduce #(inc-count %1 %2) {} items-to-count))
 
 (defn least-frequent [counts-map] (key (apply min-key val counts-map)))
 
